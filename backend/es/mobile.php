c4c31c4c963a82dbe5f827172b97219a42b2545f
Gonzalo SR <gonzalo@gonzalosr.com>
2011-02-28 18:15:01 +0100

    terminado mobile

--- backend/es/mobile.php
+++ backend/es/mobile.php
@@ -15,19 +15,19 @@
  */
 
 $__LANG = array (
-	'invalidmobileflagtype' => 'ERROR: Invalid flag type (%s) specified - unable to change flag type',
-	'mobemailfieldemptyorinvalid' => 'ERROR: Email field is invalid or empty',
-	'mobnewticketerrmsg' => 'While processing the new ticket data for subject \'%s\', the following occurred: ',
-	'mobnewticketnopermission' => 'Permission to create a new ticket has been denied.',
+	'invalidmobileflagtype' => 'ERROR: Se ha especificado un tipo de bandera (%s) no válido - no se ha podido cambiar el tipo de bandera',
+	'mobemailfieldemptyorinvalid' => 'ERROR: El campo email no es válido o está vacío',
+	'mobnewticketerrmsg' => 'Mientras se procesaban los nuevos datos del ticket con asunto \'%s\', ha ocurrido lo siguiente: ',
+	'mobnewticketnopermission' => 'Se ha denegado el permiso para crear un nuevo ticket.',
 
-	'invalidmobilexml' => 'ERROR: Invalid XML received',
-	'invalidmobileticket' => 'ERROR: Invalid ticket (%s) specified. Make sure that the ticket exists and that you have permissions for the department it resides in',
-	'notassignedmobileticket' => 'ERROR: You do not have permissions to the department that the ticket (%s) resides in',
-	'invalidmobilestaff' => 'ERROR: Invalid staff member (%s) specified - unable to assign ticket',
-	'invalidmobiledep' => 'ERROR: Invalid department (%s) specified - unable to move ticket',
-	'invalidmobilestatus' => 'ERROR: Invalid ticket status (%s) specified - unable to change status',
-	'invalidmobilepriority' => 'ERROR: Invalid ticket priority (%s) specified - unable to change priority',
-	'nopermdelkm' => 'ERROR: Failed to delete ticket %s - access denied',
-	'kmnoticketexist' => 'ERROR: Ticket %s does not exist in the database',
+	'invalidmobilexml' => 'ERROR: El XML recibido no es válido',
+	'invalidmobileticket' => 'ERROR: Se ha especificado un ticket no válido (%s). Asegúrese de que el ticket existe y de que tiene permisos para el departamento en el que se encuentra',
+	'notassignedmobileticket' => 'ERROR: No tiene permisos para el departamento en el que se encuentra el ticket (%s)',
+	'invalidmobilestaff' => 'ERROR: Se ha especificado un operador (%s) no válido - imposible asignar el ticket',
+	'invalidmobiledep' => 'ERROR: Se ha especificado un departamento (%s) no válido - imposible mover el ticket',
+	'invalidmobilestatus' => 'ERROR: Se ha especificado un estado (%s) del ticket no válido - imposible cambiar el estado del ticket',
+	'invalidmobilepriority' => 'ERROR: Se ha especificado una prioridad del ticket (%s) no válida - imposible cambiar la prioridad del ticket',
+	'nopermdelkm' => 'ERROR: No se ha podido eliminar el ticket %s - acceso denegado',
+	'kmnoticketexist' => 'ERROR: El ticket %s no existe en la base de datos',
 );
 ?>
