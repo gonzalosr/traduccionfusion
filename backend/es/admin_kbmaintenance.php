04533556025b7c55e599d63472db95638ba5bad4
Gonzalo SR <gonzalo@gonzalosr.com>
2011-07-12 20:55:58 +0200

    Nueva rama para versión  4.01.240

---
+++ backend/es/admin_kbmaintenance.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * =======================================
+ * ###################################
+ * SWIFT Framework
+ *
+ * @package	SWIFT
+ * @author	Kayako Infotech Ltd.
+ * @copyright	Copyright (c) 2001-2009, Kayako Infotech Ltd.
+ * @license	http://www.kayako.com/license
+ * @link		http://www.kayako.com
+ * @filesource
+ * ###################################
+ * =======================================
+ */
+
+$__LANG = array (
+	// ======= BEGIN v4 LOCALES =======
+	'tabkbindex' => 'Reconstruír índice de búsqueda',
+	'knowledgebase' => 'Biblioteca',
+	'maintenance' => 'Mantenimiento',
+	'rebuild' => 'Reconstruír',
+	'articlesperpass' => 'Tamaño de pasada',
+	'desc_articlesperpass' => 'Introduzca el número de artículos a procesar en cada pasada <i>(por defecto = 100).</i><br/><br/>Debido a limitaciones de memoria y uso de procesador, no se recomienda que este valor sea mayor de cien (100).<br/><br/><font color="cc3300">Nota: Durante el procesamiento <i>no</i> se actualizará el contador de tiempo, pero el proceso seguirá corriendo.</font>',
+	'reindexheader' => '<b>Proceso de reconstrucción</b>',
+	'totalreplies' => 'Total de artículos en la base de datos:',
+	'articlesprocesed' => 'Artículos procesados:',
+	'timeelapsed' => 'Tiempo transcurrido:',
+	'timeremaining' => 'Tiempo restante (estimado):',
+);
+?>
